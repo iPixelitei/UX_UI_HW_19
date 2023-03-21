91f2e5527464b1cf8335f7810a655a829cdf51a3
iPixelitei <mayala.aly@gmail.com>
2023-03-20 21:24:53 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,41 +1,61 @@
-function fade(img) {
+function fade() {
  $(".preloader").fadeOut("slow");
 }
  setTimeout(fade, 3000);
   
   //carousel images
-  $(document).ready(function() {
-    const carousel = $('.workGrid');
-    const carouselContainer = carousel.find('.carousel-container');
-    const carouselImages = carouselContainer.find('img');
-    let currentImageIndex = 0;
+  const carousel1 = document.querySelector(".carousel1 ul");
+  const images1 = document.querySelectorAll(".carousel1 li");
+  let currentImageIndex1 = 0;
   
-    // show the current image and hide the others
-    function showImage(index) {
-      carouselImages.each(function(i) {
-        if (i === index) {
-          $(this).fadeIn();
-        } else {
-          $(this).fadeOut();
-        }
-      });
+  const carousel2 = document.querySelector(".carousel2 ul");
+  const images2 = document.querySelectorAll(".carousel2 li");
+  let currentImageIndex2 = 0;
+  
+  function displayCurrentImage1() {
+    images1.forEach(image => {
+      image.style.display = "none";
+    });
+  
+    images1[currentImageIndex1].style.display = "block";
+  }
+  
+  function displayCurrentImage2() {
+    images2.forEach(image => {
+      image.style.display = "none";
+    });
+  
+    images2[currentImageIndex2].style.display = "block";
+  }
+  
+  function nextImage1() {
+    currentImageIndex1++;
+  
+    if (currentImageIndex1 >= images1.length) {
+      currentImageIndex1 = 0;
     }
   
-    // go to the next image
-    function nextImage() {
-      currentImageIndex++;
-      if (currentImageIndex >= carouselImages.length) {
-        currentImageIndex = 0;
-      }
-      showImage(currentImageIndex);
+    displayCurrentImage1();
+  }
+  
+  function nextImage2() {
+    currentImageIndex2++;
+  
+    if (currentImageIndex2 >= images2.length) {
+      currentImageIndex2 = 0;
     }
   
-    // start the slideshow
-    setInterval(nextImage, 5000);
+    displayCurrentImage2();
+  }
   
-    // show the first image initially
-    showImage(currentImageIndex);
-  });
+  nextImage1();
+  setInterval(nextImage1, 5000);
+  
+  nextImage2();
+  setInterval(nextImage2, 5000);
+  
+  
+ 
   
   // smooth scrolling
   $(document).ready(function() {
